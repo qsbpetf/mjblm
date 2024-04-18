cc64bc0a4e0936e8791e92fd6757b57c9938a5ad
Peter Friberg <peter.friberg@metaforce.se>
2024-04-18 15:35:59 +0200

    Add new components and update API version
    Two new components, bidragsraderModal and applicationTree, are added including the corresponding .css, .html, .js, and xml files. API version updated in ApplicationFormsService.cls-meta.xml from 56.0 to 60.0. The ApplicationFormsService is also modified to bring new fields from Barnen__r and Bidragsrader__r objects. Adjustments are made in the .gitignore file.

---
+++ force-app/main/default/lwc/applicationTree/applicationTree.js
@@ -0,0 +1,194 @@
+/**
+ * Created by peterfriberg on 2024-04-17.
+ */
+
+import { LightningElement, api, wire, track } from 'lwc';
+import apexGetApplication from '@salesforce/apex/ApplicationFormsController.getApplication';
+
+const actions = [
+    {
+        label: 'Redigera',
+        name: 'edit_details',
+        iconName: 'utility:view_details'
+    }
+];
+
+export default class ApplicationTree extends LightningElement {
+
+    @api recordId;
+    @track data = [];
+    @track dataById = {};
+    @track recordById = {};
+
+    @track isModalOpen = false; // Used to control the visibility of modal
+    @track selectedItem = { id: '' }; // Used to store the clicked row data handleRowClick(event) {
+    @track content;
+
+    actions = actions;
+    @track record;
+    @track recId = '';
+
+    COLUMNS = [
+        {
+            type: 'url',
+            fieldName: 'url',
+            label: 'Datapost',
+            typeAttributes: {
+                label: { fieldName: 'name' }
+            }
+        },
+        {
+            type: 'text',
+            fieldName: 'firstName',
+            label: 'Förnamn',
+        },
+        {
+            type: 'text',
+            fieldName: 'lastName',
+            label: 'Efternamn',
+        },
+        {
+            type: 'text',
+            fieldName: 'birthYear',
+            label: 'Födelseår',
+        },
+        {
+            type: 'text',
+            fieldName: 'category',
+            label: 'Kategori',
+        },
+        {
+            type: 'text',
+            fieldName: 'subCategory',
+            label: 'Underkategori',
+        },
+        {
+            type: 'currency',
+            fieldName: 'request',
+            label: 'Begärt',
+        },
+        {
+            type: 'currency',
+            fieldName: 'granted',
+            label: 'Beviljat',
+        },
+        {
+            type: 'text',
+            fieldName: 'paymentType',
+            label: 'Betalningstyp',
+        },
+        {
+            type: 'text',
+            fieldName: 'description',
+            label: 'Beskrivning'
+        },
+        {
+            type: 'button',
+            label: 'Detaljer',
+            typeAttributes: {
+                iconName: 'utility:preview',
+                name: 'edit_details',
+                label: 'Redigera',
+                title: 'Redigera',
+                variant: 'base'
+            },
+            initialWidth: 100
+        }
+    ];
+
+    get cardTitle() {
+        return 'Ansökan: ' + (this.record ? '' + this.record.Name : '');
+    }
+
+    // Method to handle the row action
+    handleRowClick(event) {
+        this.selectedItem = event.detail.row;
+        const action = event.detail.action;
+        console.log('Action: ', action.name);
+        console.log('Selected Row: ', this.selectedItem, JSON.stringify(this.selectedItem, null, 2));
+        this.content = JSON.stringify(this.selectedItem, null, 2);
+
+        if (this.selectedItem.level === 2) {
+            // Open Bidragsrader__c record modal
+            this.openModal(this.selectedItem.id);
+        }
+    }
+
+    // Method to open the modal
+    openModal(recId) {
+        this.recId = recId;
+        this.isModalOpen = true;
+        console.log('openModal() calling for record id: ' + this.recId);
+    }
+
+    // Method to close the modal
+    closeModal(event) {
+        this.isModalOpen = false;
+        // check event for closed modal and saved pressed
+        if (event.detail && event.detail.saved) {
+            console.log('Record was saved');
+            // You can perform additional actions here if needed, such as refreshing data
+            console.log('Record was saved: ', JSON.stringify(event.detail.data, null, 2));
+            // let childNode = this.dataById[event.detail.data.id];
+            // childNode.description = event.detail.data.fields.Annat_Beskrivning__c.value;
+            // console.log('Updated childNode: ', JSON.stringify(childNode, null, 2));
+            let rec = this.recordById[event.detail.data.id];
+            rec.Annat_Beskrivning__c = event.detail.data.fields.Annat_Beskrivning__c.value;
+            console.log('Updated record: ', JSON.stringify(rec, null, 2));
+            this.data = this.buildTree();
+        }
+        this.isModalOpen = false;
+    }
+
+    connectedCallback() {
+        this.getApplication(this.recordId);
+    }
+
+    async getApplication(applicationId) {
+        this.record = await apexGetApplication({ formId: applicationId });
+        console.log('Application: ', this.record, JSON.stringify(this.record, null, 2));
+        this.data = this.buildTree();
+    }
+
+    buildTree() {
+        let treeData = [];
+        let barn = {};
+
+        this.record.Barnen__r.forEach(child => {
+            let childNode = {
+                id: child.Id,
+                name: child.Name,
+                url: "/application/s/detail/" + child.Id,
+                firstName: child.XC_Fornamn__c,
+                lastName: child.XC_Efternamn__c,
+                birthYear: child.XC_Fodelsear__c,
+                request: 0,
+                granted: 0,
+                _children: []
+            };
+            barn[child.Id] = childNode;
+            treeData.push(childNode);
+        });
+
+        this.record.Bidragsrader__r.forEach(child => {
+            let childNode = {
+                id: child.Id,
+                name: child.Name,
+                // url: "/application/s/detail/" + child.Id,
+                category: child.Kategori__c,
+                subCategory: child.Underkategori__c,
+                request: child.Ans_kt_V_rde_Kontanter_Presentkort__c,
+                granted: child.Beviljat_V_rde_Presentkort_Kontanter__c,
+                paymentType: child.Kontanter_Presentkort__c,
+                description: child.Annat_Beskrivning__c
+            };
+            barn[child.Barnet_ApplicationEntry__c]._children.push(childNode);
+            barn[child.Barnet_ApplicationEntry__c].request += childNode.request || 0;
+            barn[child.Barnet_ApplicationEntry__c].granted += childNode.granted || 0;
+            this.dataById[child.Id] = childNode;
+            this.recordById[child.Id] = child;
+        });
+
+        return treeData;
+    }
+}
---
+++ force-app/main/default/lwc/bidragsraderModal/bidragsraderModal.js
@@ -0,0 +1,84 @@
+import { LightningElement, api, track, wire } from 'lwc';
+import { getRecord } from "lightning/uiRecordApi";
+import NAME_FIELD from '@salesforce/schema/Bidragsrader__c.Name';
+import KATEGORI_FIELD from '@salesforce/schema/Bidragsrader__c.Kategori__c';
+import UNDERKAT_FIELD from '@salesforce/schema/Bidragsrader__c.Underkategori__c';
+import ANSOKT_FIELD from '@salesforce/schema/Bidragsrader__c.Ans_kt_V_rde_Kontanter_Presentkort__c';
+import BEVILJAT_FIELD from '@salesforce/schema/Bidragsrader__c.Beviljat_V_rde_Presentkort_Kontanter__c';
+import KONTANTPKORT_FIELD from '@salesforce/schema/Bidragsrader__c.Kontanter_Presentkort__c';
+import BESKRIVNING_FIELD from '@salesforce/schema/Bidragsrader__c.Annat_Beskrivning__c';
+
+export default class BidragsraderModal extends LightningElement {
+
+    @api isOpen = false;
+    @api
+    get recordId() {
+        return this.privateRecordId;
+    }
+    set recordId(value) {
+        console.log('set recordId() called with value: ' + value);
+        this.privateRecordId = value;
+        this.initializedRecordId = value;
+        if (value) {
+            this.openModal();
+        }
+    }
+
+    privateRecordId;
+    @track initializedRecordId = '';
+
+    // wire to get the Bidragsrader__c record
+    @wire(getRecord, {
+        recordId: '$initializedRecordId',
+        fields: [NAME_FIELD, KATEGORI_FIELD, UNDERKAT_FIELD, ANSOKT_FIELD, BEVILJAT_FIELD, KONTANTPKORT_FIELD, BESKRIVNING_FIELD]
+    })
+    wiredRecord({ error, data }) {
+        console.log('wiredRecord() called with data: ' + data);
+        if (data) {
+            this.record = data;
+            this.error = undefined;
+            console.log('Record data:', JSON.stringify(data));
+        } else if (error) {
+            this.error = error;
+            this.record = undefined;
+            console.error('Error fetching record:', JSON.stringify(error));
+        }
+    }
+
+    // To open the modal
+    @api openModal() {
+        this.isOpen = true;
+        console.log('openModal() called for record id: ' + this.recordId);
+    }
+
+    // To close the modal
+    handleClose() {
+        this.isOpen = false;
+        this.dispatchEvent(new CustomEvent('close'));
+    }
+
+    // Handling saving the data - this utilizes the built-in submit functionality of lightning-record-edit-form
+    handleSave() {
+        console.log('handleSave() called');
+        this.template.querySelector('lightning-record-edit-form').submit();
+        this.isOpen = false; // Optionally close the modal on save
+        this.dispatchEvent(new CustomEvent('close', {
+            detail: {
+                saved: true,
+                data: this.record
+            }
+        }));
+    }
+
+    handleSuccess(event) {
+        const updatedRecord = event.detail; // This contains the fields that were submitted.
+        console.log('handleSuccess() called' + JSON.stringify(event.detail, null, 2));
+        this.isOpen = false;
+        this.dispatchEvent(new CustomEvent('close', {
+            detail: {
+                saved: true,
+                data: updatedRecord
+            }
+        }));
+    }
+}
