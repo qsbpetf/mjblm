4662496c98219dc261514624e414d1829508ce1a
Alexander Urtti <alexander.urtti@sofigate.com>
2024-09-12 18:02:44 +0200

    Added text and Ansökningskategori on Application form
    Added functionalltiy to flag for different category on form to make reports easier and find blind spots

--- force-app/main/default/lwc/application/application.js
+++ force-app/main/default/lwc/application/application.js
@@ -266,7 +266,22 @@ export default class Application extends LightningElement {
     }
 
     selectLf(evt) {
+        
         this.selectedLF = this.lfs.find(lf => lf.Id === evt.target.value);
+        this.setApplicationCategory(this.selectedLF.XC_LF_typ__c);
+        //console.log(JSON.stringify(this.form.Ans_kningskategori__c));
+    }
+
+    setApplicationCategory(LfType){
+        if (LfType === 'Majblommegrupp') {
+            this.form.Ans_kningskategori__c = 'Majblommegrupp';
+        }
+        else if (LfType === 'Saknas') {
+            this.form.Ans_kningskategori__c = 'Vit Fläck';
+        }
+        else if (LfType === 'Lokalförening') {
+            this.form.Ans_kningskategori__c = 'Lokalförening';
+        }
     }
 
     get certifierRequired() {
